d12beefc0ea9f20a2f746104782bcc045c67ddc0
20015223 <20015223@hq.corp.leroymerlin.com@X504340.hq.corp.leroymerlin.com>
2023-09-14 14:37:23 +0200

    feat: filter with name of animal done

--- app.js
+++ app.js
@@ -4,23 +4,40 @@ const { program } = require('commander');
 const {data}  = require('./data')
 
 
+function filterAnimal(filterAnimal){
+    const transformedData = [];
+
+    data.forEach(location => {
+        location.people.forEach(person => {
+          person.animals.forEach(animal => {
+
+            if (animal.name.includes(filterAnimal.filter)) {
+                transformedData.push({
+                    name: location.name,
+                    people:[
+                        {
+                            name: person.name,
+                            animals:[{
+                                name: animal.name
+                            }]
+                        }
+                    ]
+                })
+            }
+          });
+        });
+      });
+      
+      return transformedData;
+
+}
+
 program
   .version('1.0.0')
   .description('Une CLI de démonstration en Node.js')
   .option('--filter <filter>', 'Filtre à appliquer')
   .action((options) => {
-    data.forEach(location => {
-        location.people.forEach(person => {
-          person.animals.forEach(animal => {
-            if (animal.name.includes(options.filter)) {
-                console.log(`Animal: ${animal.name}`);
-            }
-          });
-        });
-      });
-    
-
-
+        console.log(JSON.stringify(filterAnimal(options)));
   });
 
 program.parse(process.argv);
