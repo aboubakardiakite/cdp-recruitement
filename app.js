1edc1d3556eab19cf0726574fc58a58a8de0d5ac
20015223 <20015223@hq.corp.leroymerlin.com@X504340.hq.corp.leroymerlin.com>
2023-09-21 09:57:02 +0200

    feat: added error management

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const { program } = require('commander');
 
 const {data}  = require('./data')
-const sentence  = 'Hello WOrld';
 
 function filterAnimal(filterAnimal,data) {
     return data.flatMap((location) =>
@@ -45,22 +44,25 @@ function filterAnimal(filterAnimal,data) {
 
 
 module.exports = {
-    sentence,
     filterAnimal,countChildren
 }
 
 
 program
-  .version('1.0.0')
+  .version('1.0.1')
   .option('--filter <filter>', 'Filter')
   .option('--count', 'count')
   .action((options) => {
-    if(options.filter)
-        console.log(filterAnimal(options.filter,data));
-    if(options.count)
-        console.log(countChildren(data));
+      try {
+          if(options.filter)
+              console.log(filterAnimal(options.filter,data));
+          if(options.count)
+              console.log(countChildren(data));
+      }catch (error) {
+          console.error("This command does not exist",error.message);
+          process.exit(1);
+      }
 
   });
-
 
 program.parse(process.argv);
